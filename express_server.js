aa79b7e3c3395e046359848dc382093dc20bbab4
Josh Sparkes <jshsparx@gmail.com>
2022-03-21 16:29:30 -0600

    Created and filled out urls_index.ejs template

--- express_server.js
+++ express_server.js
@@ -8,6 +8,10 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
